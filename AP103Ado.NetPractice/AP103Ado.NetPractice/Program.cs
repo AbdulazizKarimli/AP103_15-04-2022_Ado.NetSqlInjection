a766995334652e3048a4fc5e710f1f2fab0037ad
Abdulaziz930 <abdulazizak@code.edu.az>
2022-04-15 13:47:44 +0400

    first commit

---
+++ AP103Ado.NetPractice/AP103Ado.NetPractice/Program.cs
@@ -0,0 +1,42 @@
+﻿using AP103Ado.NetPractice.Utils;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace AP103Ado.NetPractice
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //a';DECLARE @TableName NVARCHAR(100);SELECT @TableName=TABLE_NAME FROM INFORMATION_SCHEMA.TABLES;DECLARE @DynSql nvarchar(max) = 'DROP TABLE ' + TableName;EXEC(@DynSql);SELECT 1 '%
+            Console.WriteLine("Search:");
+            string search = Console.ReadLine();
+            await GetAllAsync(search);
+        }
+
+        public async static Task GetAllAsync(string search)
+        {
+            using (SqlConnection connection = new SqlConnection(Constants.connectionString))
+            {
+                connection.Open();
+                //string command = $"SELECT * FROM Students WHERE Fullname LIKE '%{search}%'";
+                string command = $"SELECT * FROM Students WHERE Fullname LIKE '%@search%'";
+                
+                using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@search", search);
+                    SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"Fullname: {reader["Fullname"]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AP103Ado.NetPractice/AP103Ado.NetPractice/Utils/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AP103Ado.NetPractice.Utils
+{
+    internal class Constants
+    {
+        public static string connectionString = "Data Source=DESKTOP-1QPG03B;Initial Catalog=AP103Course;"
+            + "Integrated Security=true;";
+    }
+}
